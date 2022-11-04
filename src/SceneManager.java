3ce7657a513bc5a2c2ba9e8331589cd775a6ef85
Michael Hill <hillm5@jacks.sfasu.edu>
2022-11-04 17:50:25 -0500

    Updated the input frame

--- src/SceneManager.java
+++ src/SceneManager.java
@@ -1,4 +1,5 @@
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import items.*;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -8,26 +9,31 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SceneManager {
 
     // strings to initialize images
     String precedingPath = "C:\\Users\\justa\\Documents\\SFA_CLASSWORK\\CSCI_3331_-_Object_Oriented_Programming_Methods\\Loot-Tracker\\bin\\images\\";
-    String[] imagePaths = new String[] { "ah", "aps", "arb", "art", "db", "dc", "dhc", "dps", "em", "ki", "md",
-            "o", "tb", "tbow" };
+    String[] imagePaths = new String[] { "dps", "aps", "tb", "dhc", "db", "ah", "art", "arb", "dc", "em", "ki",
+            "tbow", "o", "md" };
     String fileExtension = ".png";
 
     // images array
     Image[] images;
 
     Button[] itemButtons = new Button[imagePaths.length];
+    Button selected;
 
     int numItems = 14;
 
@@ -49,98 +55,6 @@ public class SceneManager {
 
         Button btBow = new Button("Create Bow");
         TestHandlerClass testhandle = new TestHandlerClass();
-
-        // Setting up the grid of buttons
-        GridPane gpane = new GridPane();
-        gpane.setHgap(25);
-
-        // Looping to actually create the buttons
-        int count = 0;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (count < numItems) {
-                    // Image Creation
-                    Image image = new Image(precedingPath + imagePaths[count] + fileExtension);
-                    ImageView imageView = new ImageView(image);
-
-                    // Button Formatting
-                    Button button = new Button();
-                    button.setMinHeight(50);
-                    button.setMinWidth(50);
-                    button.setGraphic(imageView);
-                    button.setAlignment(Pos.CENTER);
-
-                    itemButtons[count] = button;
-
-                    // button.setTranslateX(100 + (j * 50));
-                    // button.setTranslateY(100 + (i * 100));
-
-                    gpane.add(button, j, i);
-
-                    count++;
-                }
-
-            }
-        }
-        count = 0;
-        bpane.setCenter(gpane);
-
-        for (iterator = 0; iterator < numItems; iterator++) {
-            itemButtons[iterator].setOnAction(e -> {
-                System.out.print(iterator);
-                Loot loot;
-                switch (iterator) {
-                    case 0:
-                        loot = new Ances_Bottom();
-                        break;
-                    case 1:
-                        loot = new Ances_Hat();
-                        break;
-                    case 2:
-                        loot = new Ances_Top();
-                        break;
-                    case 3:
-                        loot = new Arcane();
-                        break;
-                    case 4:
-                        loot = new Buckler();
-                        break;
-                    case 5:
-                        loot = new Claws();
-                        break;
-                    case 6:
-                        loot = new Dex();
-                        break;
-                    case 7:
-                        loot = new DHCB();
-                        break;
-                    case 8:
-                        loot = new Dinh();
-                        break;
-                    case 9:
-                        loot = new Elder_Maul();
-                        break;
-                    case 10:
-                        loot = new Kodai_Insignia();
-                        break;
-                    case 11:
-                        loot = new Twisted_Bow();
-                        break;
-                    case 12:
-                        loot = new Twisted_Bow();
-                        break;
-                    case 13:
-                        loot = new Twisted_Bow();
-                        break;
-                    case 14:
-                        loot = new Twisted_Bow();
-                        break;
-
-                }
-
-            });
-        }
 
         // Buttons interactions to add objects
         btBow.setOnAction(e -> {
@@ -171,8 +85,14 @@ public class SceneManager {
         });
         btCompare.setPadding(new Insets(5));
 
+        Button btSubmit = new Button("Submit");
+        btSubmit.setOnAction(e -> {
+            submitScene(primaryStage);
+        });
+        btSubmit.setPadding(new Insets(5));
+
         // Adding objects to scene and displaying scene
-        hbox.getChildren().addAll(btBow, btKodai, btCompare);
+        hbox.getChildren().addAll(btBow, btKodai, btCompare, btSubmit);
         hbox.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(bpane, 400, 500);
@@ -186,8 +106,173 @@ public class SceneManager {
         BorderPane bpane = new BorderPane();
         HBox hbox = new HBox();
 
-        Text t = new Text("This is the new scene!");
-        // bpane.setCenter(t);
+        // Instruction label formatting
+        Label instructions = new Label(
+                "Instructions: Select which item you wish to add and input the kill count it was obtained at. Toggle the checkboxes to correspond to the conditions that you obtained your loot in.");
+        instructions.setWrapText(true);
+        instructions.setTextAlignment(TextAlignment.CENTER);
+        instructions.setTranslateX(10);
+        instructions.setMaxWidth(580);
+        bpane.setTop(instructions);
+
+        // Setting up the options in the middle of the frame;
+        VBox vbox = new VBox(25);
+        vbox.setTranslateY(220);
+
+        GridPane centerGridPane = new GridPane();
+
+        centerGridPane.setTranslateX(50);
+
+        centerGridPane.setHgap(25);
+        centerGridPane.setVgap(25);
+
+        // kill count field
+        TextField killCountField = new TextField();
+        killCountField.setMaxWidth(100);
+
+        // checkboxes
+        CheckBox challengeBox = new CheckBox();
+        CheckBox soloBox = new CheckBox();
+        CheckBox personalBox = new CheckBox();
+
+        // submit button
+        Button submitButton = new Button("Submit");
+        submitButton.setTranslateX(182);
+        submitButton.setMinWidth(100);
+
+        // corresponding labels
+        Label killCountLabel = new Label("Kill Count Obtained");
+        Label challengeLabel = new Label("Challenge Mode");
+        Label soloLabel = new Label("Obtained Solo");
+        Label personalLabel = new Label("Personally Obtained");
+
+        centerGridPane.add(killCountLabel, 0, 0);
+        centerGridPane.add(killCountField, 1, 0);
+        centerGridPane.add(challengeLabel, 0, 1);
+        centerGridPane.add(challengeBox, 1, 1);
+        centerGridPane.add(soloLabel, 0, 2);
+        centerGridPane.add(soloBox, 1, 2);
+        centerGridPane.add(personalLabel, 0, 3);
+        centerGridPane.add(personalBox, 1, 3);
+        // centerGridPane.add(submitButton, 1, 4);
+
+        vbox.getChildren().add(0, centerGridPane);
+        vbox.getChildren().add(1, submitButton);
+        bpane.setCenter(vbox);
+
+        // Setting up the grid of buttons
+        GridPane gpane = new GridPane();
+        gpane.setTranslateX(10);
+        gpane.setTranslateY(10);
+        gpane.setHgap(10);
+        gpane.setVgap(10);
+
+        // Looping to actually create the buttons
+        int count = 0;
+        bpane.setLeft(gpane);
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (count < numItems) {
+                    // Image Creation
+                    Image image = new Image(precedingPath + imagePaths[count] + fileExtension);
+                    ImageView imageView = new ImageView(image);
+
+                    // Button Formatting
+                    Button button = new Button();
+                    button.setMinHeight(50);
+                    button.setMinWidth(50);
+                    button.setGraphic(imageView);
+                    button.setAlignment(Pos.CENTER);
+
+                    itemButtons[count] = button;
+
+                    // button.setTranslateX(100 + (j * 50));
+                    // button.setTranslateY(100 + (i * 100));
+
+                    gpane.add(button, j, i);
+
+                    count++;
+                }
+
+            }
+        }
+        count = 0;
+
+        /*
+         * for (iterator = 0; iterator < numItems; iterator++) {
+         * itemButtons[iterator].setOnAction(e -> {
+         * System.out.print(iterator);
+         * Loot loot;
+         * switch (iterator) {
+         * case 0:
+         * loot = new Ances_Bottom();
+         * break;
+         * case 1:
+         * loot = new Ances_Hat();
+         * break;
+         * case 2:
+         * loot = new Ances_Top();
+         * break;
+         * case 3:
+         * loot = new Arcane();
+         * break;
+         * case 4:
+         * loot = new Buckler();
+         * break;
+         * case 5:
+         * loot = new Claws();
+         * break;
+         * case 6:
+         * loot = new Dex();
+         * break;
+         * case 7:
+         * loot = new DHCB();
+         * break;
+         * case 8:
+         * loot = new Dinh();
+         * break;
+         * case 9:
+         * loot = new Elder_Maul();
+         * break;
+         * case 10:
+         * loot = new Kodai_Insignia();
+         * break;
+         * case 11:
+         * loot = new Twisted_Bow();
+         * break;
+         * case 12:
+         * loot = new Twisted_Bow();
+         * break;
+         * case 13:
+         * loot = new Twisted_Bow();
+         * break;
+         * case 14:
+         * loot = new Twisted_Bow();
+         * break;
+         * 
+         * }
+         * 
+         * });
+         * }
+         */
+
+        for (int i = 0; i < itemButtons.length; i++) {
+            Button active = itemButtons[i];
+            active.setOnAction(e -> {
+                if (selected != null) {
+                    active.setDisable(true);
+                    selected.setDisable(false);
+                    selected = active;
+                    killCountField.requestFocus();
+                } else {
+                    active.setDisable(true);
+                    selected = active;
+                    killCountField.requestFocus();
+                }
+
+            });
+        }
 
         Button btReturn = new Button("Return to main menu");
         // SwitchHandlerClass returnHandle = new SwitchHandlerClass(primaryStage, this,
@@ -198,7 +283,7 @@ public class SceneManager {
         bpane.setBottom(btReturn);
         bpane.setAlignment(btReturn, Pos.CENTER);
 
-        Scene scene = new Scene(bpane, 400, 500);
+        Scene scene = new Scene(bpane, 600, 800);
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.setResizable(false);
